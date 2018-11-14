b0fb88b39ea9d8b1e2f44c2951371a83a1850cb3
Infin8gamer1 <Jacob@mjhsoftware.com>
2018-11-13 22:04:36 -0800

    added code so that the robot will turn in the direction that the mineral is

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MineralKnockOffPaths/MineralLKnockOff_G.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MineralKnockOffPaths/MineralLKnockOff_G.java
@@ -4,6 +4,6 @@ import org.firstinspires.ftc.teamcode.RobotCommands;
 
 public class MineralLKnockOff_G {
     public static void Run(RobotCommands robotCommands){
-
+        robotCommands.DriveForTicks(-2000,2000);
     }
 }
--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MineralKnockOffPaths/MineralRKnockOff_S.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/MineralKnockOffPaths/MineralRKnockOff_S.java
@@ -4,6 +4,6 @@ import org.firstinspires.ftc.teamcode.RobotCommands;
 
 public class MineralRKnockOff_S {
     public static void Run(RobotCommands robotCommands){
-
+        robotCommands.DriveForTicks(2000,-2000);
     }
 }
